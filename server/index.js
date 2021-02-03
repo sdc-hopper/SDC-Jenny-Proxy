2e2d2ef5317ba96d2bcc6ee23d1c4e71706dc0e6
Alek Ortiz <aleksebastiano@gmail.com>
2021-02-03 00:55:58 -0600

    Proxy server running services

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
 const port = 3000;
 const app = express();
 
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/../public`));
+
+app.use('/:id', express.static(`${__dirname}/../public`));
+
+// app.get('/:id', (req, res) => {
+//   const file = path.join(__dirname, '../public/index.html');
+//   res.sendFile(file);
+// });
+
+
 
 
 app.listen(port, () => console.log(`listening on port ${port}`));
